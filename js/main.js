e4fadc5d9f60050a197ba41366d6a0118602c467
ieeah <lbnfnc90@gmail.com>
2021-11-03 17:40:24 +0100

    done grid with eventlistener

--- js/main.js
+++ js/main.js
@@ -24,6 +24,54 @@ let rowSquares = 0;
 
 
 
+// al click bisogna creare n div.square in base al livello di difficoltà selezionato dall'utente
+playBtn.addEventListener('click', function() {
+    //reset del contenuto di wrap_grid
+    wrapGrid.innerHTML = '';
+    // ottenimento del valore di difficoltà
+    const size = selector.value;
+    switch (size) {
+        case '1':
+            totSquares = 100;
+            rowSquares = 10;
+            break;
+
+        case '2':
+            totSquares = 81;
+            rowSquares = 9;
+            break;
+
+        case '3':
+            totSquares = 49;
+            rowSquares = 7;
+    }
+    // creazione dei div in base al livello di difficoltà
+    const square = createSquare(totSquares, rowSquares);
+
+    // generare 16 numeri casuali e inserirli nell'array se non presenti
+    const maxBombs = 16;
+    const bombList = bombs(maxBombs, totSquares);
+});
+
+
+
+
+
+
+
+
+
+/*Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco 
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l’utente clicca su ogni cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve scoprire tutte le bombe e comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+Ad esempio:
+Di cosa ho bisogno per generare i numeri delle bombe?
+Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+*/
 
 
 
@@ -50,83 +98,49 @@ let rowSquares = 0;
 
 
 
+// funzioni
+function createSquare(num, rowSquares) {
 
+    for (i = 1; i <= num; i++) {
+        //creazione dell'elemento
+        const square = document.createElement('div');
+        // aggiunta della classe square
+        square.classList.add('square');
+        // all'interno inseriamo il numero di index
+        square.append(i);
+        // inseriamo le dimensioni di ogni singola cella
+        square.style.width = `calc(100% / ${rowSquares})`;
+        square.style.height = `calc(100% / ${rowSquares})`;
+        //inseriamo il listener al click
+        square.addEventListener('click', function() {
+            //aggiungiamo la classe active alle celle al click
+            square.classList.add('active');
+        });
+            //inserire la cella creata nella griglia
+            wrapGrid.append(square);
+    }
+return createSquare;
+}
 
+function bombs(maxlength, nmax) {
+// creare un array fantasma che mi tenga traccia dei numeri creati
+// finché il numero di elementi nell'array non coincide con il numero di bombe di cui ho bisogno (16)
+// generare numeri random
+// controllare che siano presenti nell'array fantasma
+// no? inserisci il numero
+// si? ricomincia il ciclo fino a ch la condizione del while non è corretta
+// return la lista, che verrà usata come lista di caselle in cui sarà presente la bomba
+    let bombs = [];
+    while (bombs.length != maxlength - 1) {
+        let n = getRangeRand(1, nmax);
+        if (!bombs.includes(n)) {
+            bombs.push(n);
+        }
+    }
 
+    return bombs;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// playBtn.addEventListener('click', function() {
-//     // al click del playbtn si azzera il contenuto di wrapGrid
-//     wrapGrid.innerHTML = '';
-
-// // ottenere il livello selezionato
-//     const size = selector.value;
-
-// // in base al livello, impostare il numero di celle totali
-// // ed il numero di celle per ogni singola riga
-//     switch (size) {
-//         case '1':
-//             totSquares = 100;
-//             rowSquares = 10;
-//             break;
-        
-//         case '2':
-//             totSquares = 81;
-//             rowSquares = 9;
-//             break;
-
-//         case '3':
-//             totSquares = 49;
-//             rowSquares = 7;
-
-//     }
-
-// // creare n elementi div con classe square, in base al numero di celle totali
-//     createSquares(totSquares);
-
-// });
-
-// // funzioni
-
-// function createSquares(num) {
-//     // creare n elementi div con classe square, in base al numero di celle totali
-//     for (i = 1; i <= num; i++) {
-//         const createSquare = document.createElement('div');
-//         createSquare.classList.add('square');
-//         // ad ogni elemento assegnare un numero, creato sequenzialmente
-//         createSquare.append(i);
-//         // impostare le dimensioni di ogni singola cella
-//         createSquare.style.width = `calc(100% / ${rowSquares})`;
-//         createSquare.style.height = `calc(100% / ${rowSquares})`;
-//         // creare un eventlistener, che al click cambi il colore di sfondo dell'elemento cliccato
-//         createSquare.addEventListener('click', function() {
-//         createSquare.classList.add('active');
-//         });
-//         // inserire la cella nel wrapGrid
-//         wrapGrid.append(createSquare);
-//     }
-// }
+function getRangeRand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
