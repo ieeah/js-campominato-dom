0364f9613216d83ec7a445893de79f07067780b2
ieeah <lbnfnc90@gmail.com>
2021-11-03 18:18:47 +0100

    sto impazzendo

--- js/main.js
+++ js/main.js
@@ -21,12 +21,16 @@ const wrapGrid = document.querySelector('.wrap_grid');
 //variables
 let totSquares = 0;
 let rowSquares = 0;
+const maxBombs = 16;
+let maxAttempts = totSquares - maxBombs;
+
 
 
 
 // al click bisogna creare n div.square in base al livello di difficoltà selezionato dall'utente
 playBtn.addEventListener('click', function() {
     //reset del contenuto di wrap_grid
+    console.log('play clicked');
     wrapGrid.innerHTML = '';
     // ottenimento del valore di difficoltà
     const size = selector.value;
@@ -46,12 +50,27 @@ playBtn.addEventListener('click', function() {
             rowSquares = 7;
     }
     // creazione dei div in base al livello di difficoltà
-    const square = createSquare(totSquares, rowSquares);
+    for (let i = 0; i < totSquares; i++) {
+        const grid = createSquare(i, totSquares);
+        wrapGrid.append(grid);
+    }
+        
 
     // generare 16 numeri casuali e inserirli nell'array se non presenti
-    const maxBombs = 16;
+
     const bombList = bombs(maxBombs, totSquares);
+    console.log('lista bombe', bombList);
+
+    // innerText dell'elemento cliccato è presente in bomblist?
+    // si? cell.classList.add('bomb');
+    // no? 
+
+    if (bombList.includes(square.innerText)) {
+        console.log('bomb clicked');
+    }
 });
+
+
 
 
 
@@ -100,26 +119,21 @@ Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dat
 
 // funzioni
 function createSquare(num, rowSquares) {
-
-    for (i = 1; i <= num; i++) {
         //creazione dell'elemento
         const square = document.createElement('div');
         // aggiunta della classe square
         square.classList.add('square');
         // all'interno inseriamo il numero di index
-        square.append(i);
+        square.append(num);
         // inseriamo le dimensioni di ogni singola cella
         square.style.width = `calc(100% / ${rowSquares})`;
         square.style.height = `calc(100% / ${rowSquares})`;
         //inseriamo il listener al click
         square.addEventListener('click', function() {
             //aggiungiamo la classe active alle celle al click
-            square.classList.add('active');
-        });
-            //inserire la cella creata nella griglia
-            wrapGrid.append(square);
-    }
-return createSquare;
+        square.classList.add('active');
+    });
+return square;
 }
 
 function bombs(maxlength, nmax) {
@@ -131,13 +145,12 @@ function bombs(maxlength, nmax) {
 // si? ricomincia il ciclo fino a ch la condizione del while non è corretta
 // return la lista, che verrà usata come lista di caselle in cui sarà presente la bomba
     let bombs = [];
-    while (bombs.length != maxlength - 1) {
+    while (bombs.length <= maxlength - 1) {
         let n = getRangeRand(1, nmax);
         if (!bombs.includes(n)) {
             bombs.push(n);
         }
     }
-
     return bombs;
 }
 
